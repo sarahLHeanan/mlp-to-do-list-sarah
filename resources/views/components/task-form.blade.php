be5d87944c1e0f670365a809cbd15cec229ead9e
Sarah Heanan <sarahlheanan@gmail.com>
2024-03-07 10:14:49 +0000

    Create form for adding task

---
+++ resources/views/components/task-form.blade.php
@@ -0,0 +1,6 @@
+<form class="task-form">
+  <div class="form-group">
+    <input type="text" class="form-control" id="task" placeholder="Insert task name">
+  </div>
+  <button type="submit" class="btn btn-primary">Add</button>
+</form>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,22 +3,24 @@
         MLP To Do List
     </x-slot>
 
-    <section class="main-content">
+    <section class="main-content tasks">
         <!-- @todo task list component -->
         
-        <div class="to-do-list">
+        <div>
             <div class="row">
-                <div class="col-md-6">
-                    To do form
+                <div class="col-md-5">
+                    <x-task-form />
                 </div>
-                <div class="col-md-6">
-                    @if(isset($tasks))
-                        @foreach ($tasks as $task)
-                            {{ $task }}
-                        @endforeach
-                    @else
-                    <p>No outstanding tasks</p>
-                    @endif
+                <div class="col-md-7">
+                    <div class="to-do-list">
+                        @if(isset($tasks))
+                            @foreach ($tasks as $task)
+                                {{ $task }}
+                            @endforeach
+                        @else
+                            <p>No outstanding tasks</p>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
