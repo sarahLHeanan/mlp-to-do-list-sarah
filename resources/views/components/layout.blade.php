3d1d0b2e2b2c59f86b6f139d2b5c2c64ade12bcb
Sarah Heanan <sarahlheanan@gmail.com>
2024-03-07 00:22:42 +0000

    Create basic responsive layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,15 +7,21 @@
     <title>{{ $title ?? 'MLP To Do' }}</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Todos</h1>
-    
-    {{ $slot }}
+    <div class="container-fluid">
+        {{ $slot }}
+    </div>
 </body>
+
+
+<footer>
+        Copyright here
+    </footer>
 
 </html>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,13 +1,34 @@
 <x-layout>
     <x-slot:title>
-        Custom Title
+        MLP To Do List
     </x-slot>
- 
-    @if(isset($tasks))
-        @foreach ($tasks as $task)
-            {{ $task }}
-        @endforeach
-    @else
-     <p>No outstanding tasks</p>
-    @endif
+
+    <header>
+        <section class="hero">
+            Hero component here, logo
+        </section>
+    </header>
+
+    <section class="main-content">
+        <!-- @todo task list component -->
+        
+        <div class="to-do-list">
+            <div class="row">
+                <div class="col-md-6">
+                    To do form
+                </div>
+                <div class="col-md-6">
+                    @if(isset($tasks))
+                        @foreach ($tasks as $task)
+                            {{ $task }}
+                        @endforeach
+                    @else
+                    <p>No outstanding tasks</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+
+
+    </section>
 </x-layout>
